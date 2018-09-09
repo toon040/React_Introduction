acdae5b9b2ff0af1be23f76a2cfaf4e2ee4b573e
toon040 <toon040@hotmail.com>
2018-09-09 10:20:42 +0700

    add Header and Footer to App

---
+++ codecasterth-react/src/Header.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+class Header extends Component {
+    render() {
+        return (
+            <div>
+              Hello Header              
+            </div>
+        );
+    }
+}
+
+export default Header;
